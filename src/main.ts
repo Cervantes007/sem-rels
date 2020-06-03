4eaa29a9e40e47af57fc64e4829249d33d25fefe
gsi-alejandro <alejandro@gsoftinnovation.com>
2020-06-03 08:24:14 -0400

    feat(greeting): cHange greetings api
    BREAKING CHANGE: description

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,2 @@
 export const logger = () => console.log('Easiest package ever');
-export const greetings=(name) => console.log(`Hello, ${name}`);
+export const greeting =(name) => console.log(`Hello, ${name}`);
